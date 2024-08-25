2c2721ae937f3e79778f90b241d035439eb3e878
myrtar <160183764+myrtar@users.noreply.github.com>
2024-08-25 13:59:19 -0400

    Add files via upload

--- 5x6_right/keymaps/myrtar/keymap.c
+++ 5x6_right/keymaps/myrtar/keymap.c
@@ -14,8 +14,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_TAB, KC_QUOT, KC_COMM, KC_DOT, KC_P, KC_Y,      KC_F, KC_G, KC_C, KC_R, KC_L, KC_SLSH, 
 	KC_LSFT, KC_A, KC_O, KC_E, KC_U, KC_I,             KC_D, KC_H, KC_T, KC_N, KC_S, KC_MINS, 
 	KC_LCTL, KC_SCLN, KC_Q, KC_J, KC_K, KC_X,          KC_B, KC_M, KC_W, KC_V, KC_Z, KC_BSLS, 
-	             TO(2), TO(1),                         KC_BTN1, KC_BTN2, 
-				 MO(2), KC_SPC,                        KC_ENT, //MO(1), 
+	             KC_WH_U, KC_WH_D,                     KC_BTN1, KC_BTN2, 
+				 MO(2), KC_SPC,                        MO(1), 
 				 KC_HOME, KC_END,                      KC_ENT, 
 				 KC_BSPC, KC_LALT,                     KC_LGUI, KC_DEL
 	),
@@ -34,11 +34,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_TRNS, KC_HOME, KC_PGUP, KC_PGDN, KC_END, KC_LBRC,    KC_RBRC, KC_TRNS, KC_INS, KC_VOLU, KC_VOLD, KC_MUTE, 
 	KC_TRNS, KC_LEFT, KC_UP, KC_DOWN, KC_RGHT, KC_LPRN,     SNIPING, KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4, KC_BTN5, 
 	KC_TRNS, KC_WH_L, KC_WH_U, KC_WH_D, KC_WH_R, DRGSCRL,   DPI_MOD, DPI_RMOD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-	KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, 
+	KC_TRNS, TG(3),    KC_TRNS, KC_TRNS, 
 	KC_TRNS, KC_TRNS,    KC_TRNS, 
 	KC_TRNS, KC_TRNS,    KC_TRNS, 
 	KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS
-	)
+	),
+	[3] = LAYOUT_5x6_right(
+    QK_GESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,            KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
+    KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,             KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
+    KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,             KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
+    KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,             KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLS,
+    KC_TRNS, KC_TRNS,                        KC_TRNS, 
+	KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, 
+	KC_TRNS, KC_TRNS,    KC_TRNS, 
+	KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS
+    )
 };
 
 #if defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
